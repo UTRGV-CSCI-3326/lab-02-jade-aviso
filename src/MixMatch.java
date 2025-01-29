18c5dc6aa7a05dccc187078d6fdd8eccf33639ec
Jade Sapphire D. Aviso <147261623+jade-aviso@users.noreply.github.com>
2025-01-29 17:47:28 +0000

    started not done

---
+++ src/MixMatch.java
@@ -0,0 +1,23 @@
+
+public class MixMatch{
+    public static void main(String [] args){
+    
+    //1
+    int vari = 25;
+    System.out.println(vari + ": Integer(int) can store 
+    whole number from -2,147,483,648 to 2,147,483,647");
+
+
+    //2-String
+    String 
+
+
+
+    }
+}
+
+//declare 8 variables
+// string char short int long float double and boolean 
+
+
+
