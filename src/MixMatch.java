37b8c7a9a40f103ef4ed4ddb061566e88642ca38
Jade Sapphire D. Aviso <147261623+jade-aviso@users.noreply.github.com>
2025-01-30 17:28:18 +0000

    almost done

--- src/MixMatch.java
+++ src/MixMatch.java
@@ -4,31 +4,36 @@ public class MixMatch{
     
     //1
     int vari = 25;
-    System.out.println(vari + ": Integer(int) can store 
-    whole number from -2,147,483,648 to 2,147,483,647");
+    System.out.println(vari + ": Integer can store whole number from -2,147,483,648 to 2,147,483,647");
 
 
     //2-String
-    String[] str ="hi";
-    System.out.println(str + "defintion")
+    String str ="hi";
+    System.out.println(str + ": defintion");
 
     //3-short
     short notTall = 3;
+    System.out.println(notTall + ": Short stores whole numbers from -32,768 to 32,787");
 
     //4-long
-    long notLong = 34567;
-
+    long notLong = 467;
+    System.out.println(notLong + ": Long stores numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 ");
+    
     //5-char
     char character = 'j';
+    System.out.println(character + "char stores a single character/letter or ASCII value");
 
     //6-double
     double two = -22354;
+    System.out.println(two  + ": double stores fractional numbers. Up to  decimal digits");
 
     //7-float
-    float rootbeer = 0.3345;
+    float rootbeer = 1.45;
+    System.out.println(rootbeer + ": stores fraction numbers. can store 6-7 decimal digits");
 
     //8-boolen
     boolean answer = true;
+    System.out.println(answer + "definition");
 
 
 
